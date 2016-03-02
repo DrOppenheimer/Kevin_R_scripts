9a389c0c6cd35dd28d46f1d66406186428d11c7a
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-03-02 15:27:45 -0600

    add setup script

---
+++ setup_r_env.r
@@ -0,0 +1,9 @@
+# setup_r_env
+source("~/git/Kevin_R_scripts/import_data.r")
+source("~/git/Kevin_R_scripts/export_data.r")
+source("~/git/Kevin_R_scripts/import_metadata.r")
+source("~/git/Kevin_R_scripts/preprocessing_tool.r")
+source("~/git/Kevin_R_scripts/calculate_pco.r")
+source("~/git/Kevin_R_scripts/render_calculated_pcoa.r")
+source("~/git/Kevin_R_scripts/heatmap_dendrogram.r")
+source("~/git/Kevin_R_scripts/calc_stats.r")
