9fabb24c5648298350568a9a9f6d217e48bdaa61
DrOppenheimer <kevin.p.keegan@gmail.com>
2016-03-03 11:43:33 -0600

    update scripts

--- calc_stats.r
+++ calc_stats.r
@@ -130,7 +130,7 @@ calc_stats <<- function(
   
   # create name for the output file
   if ( identical(file_out, "default") ){
-      if is.na(colnames(metadata_matrix)[metadata_column]){
+      if ( is.na(colnames(metadata_matrix)[metadata_column]) == TRUE ){
           file_out = paste(data_table, ".", stat_test, ".", metadata_column, ".STATS_RESULTS.txt", sep="", collapse="")
       }else{
           file_out = paste(data_table, ".", stat_test, ".", colnames(metadata_matrix)[metadata_column], ".STATS_RESULTS.txt", sep="", collapse="")
