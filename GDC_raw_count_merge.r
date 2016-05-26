d5701241682de8affa58e0d782195340850621f1
Kevin P Keegan <kevin@Kevins-Macintosh.local>
2016-05-26 15:04:31 -0500

    adding GDC_raw_count_merge.r

--- GDC_raw_count_merge.r
+++ GDC_raw_count_merge.r
@@ -14,7 +14,7 @@ GDC_raw_count_merge <- function( id_list="my_id_list", my_rot="no", debug=FALSE)
     for ( i in 1:length(my_ids) ){
         print(paste("First read: ", i))
         my_data <- data.matrix(read.table(file=id_list, row.names=1, header=TRUE, sep="\t", comment.char="", quote="", check.names=FALSE))
-        my_keys <- unique( rownames(my_data), my_keys )
+        my_keys <- unique( c(rownames(my_data), my_keys) )
         if(debug==TRUE){print("num keys: ", length(my_keys))}
     }
 
