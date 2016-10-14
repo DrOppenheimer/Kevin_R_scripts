dd47c47632f4ade2c4b7c5ff27b35e3160a194cd
Kevin P Keegan <kevin@Kevins-Macintosh.local>
2016-10-14 00:15:17 -0500

    update combine matrices

--- combine_matrices.r
+++ combine_matrices.r
@@ -1,4 +1,4 @@
-combine_matrices_by_column <- function(matrix1, matrix2, export=NA, pseudo_fudge=10000, from_file=FALSE, order_rows=TRUE, order_columns=TRUE){
+combine_matrices_by_column <- function(matrix1, matrix2, export=NA, use_fudge=FALSE, pseudo_fudge=10000, na_2=NA, from_file=FALSE, order_rows=TRUE, order_columns=TRUE){
 
     # import data from file if that option is selected
     if(from_file==TRUE){
@@ -13,8 +13,15 @@ combine_matrices_by_column <- function(matrix1, matrix2, export=NA, pseudo_fudge
     rownames(comb_matrix) <- comb_matrix$Row.names
     comb_matrix$Row.names <- NULL
     colnames(comb_matrix) <- c(colnames(matrix1), colnames(matrix2))
-    pseudo_count <- min(comb_matrix, na.rm=TRUE)/pseudo_fudge # find the min real value; that num/pseudo_fudge = pseudo_count value
-    comb_matrix[is.na(comb_matrix)] <- pseudo_count # replace NA with pseudo_count
+    if ( is.na(na_2)==FALSE ){
+        comb_matrix[is.na(comb_matrix)] <- na_2 # replace NA with pseudo_count
+    }
+    if ( use_fudge==TRUE ){
+        pseudo_count <- min(comb_matrix, na.rm=TRUE)/pseudo_fudge # find the min real value; that num/pseudo_fudge = pseudo_count value
+        comb_matrix[is.na(comb_matrix)] <- pseudo_count # replace NA with pseudo_count
+    }
+
+    
     
     # order columns
     if( order_rows==TRUE){
@@ -29,7 +36,7 @@ combine_matrices_by_column <- function(matrix1, matrix2, export=NA, pseudo_fudge
     }
 
     if( is.na(export)==FALSE ){
-        output_name <- gsub(" ", "", paste(export, ".merged_data.txt")
+        output_name <- gsub(" ", "", paste(export, ".merged_data.txt"))
         export_data(comb_matrix, output_name)
     }
     
